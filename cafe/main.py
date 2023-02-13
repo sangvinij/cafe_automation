c48c4ea7f4d6c05a65904046e0a35a79ea769780
Artyom_Vorozhbyanov <artyom.vorozhbyanov@gmail.com>
2023-02-13 19:23:30 +0300

    run.py modified

--- cafe/main.py
+++ cafe/main.py
@@ -5,7 +5,3 @@ from kivymd.uix.label import MDLabel
 class MyApp(MDApp):
     def build(self):
         return MDLabel(text='Label', halign='center')
-
-
-if __name__ == '__main__':
-    MyApp().run()
