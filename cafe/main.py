8d3e3a22d2924eb097567df37290f1282c78ccac
Artyom_Vorozhbyanov <artyom.vorozhbyanov@gmail.com>
2023-02-17 13:32:42 +0300

    Создан черновик интерфейса главной страницы меню

--- cafe/main.py
+++ cafe/main.py
@@ -1,11 +1,32 @@
+import webbrowser
+
+from kivy.lang import Builder
+
 from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
+from kivymd.uix.tab import MDTabsBase
+from kivymd.uix.floatlayout import MDFloatLayout
 
 
-class MyApp(MDApp):
+class Tab(MDFloatLayout, MDTabsBase):
+    pass
+
+
+class CafeApp(MDApp):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.screen = Builder.load_file('cafe/main.kv')
+
     def build(self):
-        return MDLabel(text='Label', halign='center')
+        self.theme_cls.theme_style = 'Light'
+        self.theme_cls.primary_palette = 'Red'
+        return self.screen
 
+    def on_tab_switch(self, instance_tabs, instance_tab, instance_tab_label, tab_text):
+        print('tab clicked! ' + tab_text)
 
-if __name__ == '__main__':
-    MyApp().run()
+    def links(self, index):
+        if index == 1:
+            webbrowser.open('http://vk.com/sangvinij')
+        else:
+            webbrowser.open('http://instagram.com/sangvinij')
